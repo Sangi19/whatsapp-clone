0df7d4f3c0ca66c0b552a43c1614b392082eda54
sangeetha-ram <sangiammu1020@gmail.com>
2021-05-12 17:44:39 +0530

    Create firebase.js
    firebase authentication is added

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZDxgxdDfItb2U86myG21RwBJIbj728OU",
+    authDomain: "whatsapp-clone-49e7c.firebaseapp.com",
+    projectId: "whatsapp-clone-49e7c",
+    storageBucket: "whatsapp-clone-49e7c.appspot.com",
+    messagingSenderId: "253081168123",
+    appId: "1:253081168123:web:1c52b5e898e9e32b07685e",
+    measurementId: "G-SHY7R679HJ"
+  };
