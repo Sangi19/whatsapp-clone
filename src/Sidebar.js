62c480e52d2730fdfeb13d8bff6f927350e0fec9
sangeetha-ram <sangiammu1020@gmail.com>
2021-05-19 21:16:06 +0530

    sidebar alignment

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,30 +1,45 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import MoreVert  from '@material-ui/icons/MoreVert'
 import ChatIcon from '@material-ui/icons/Chat';
+import { Avatar, IconButton } from '@material-ui/core';
 import './Sidebar.css';
+import { SearchOutlined } from '@material-ui/icons';
 
 
 function Sidebar() {
     return (
         <div className="sidebar">
-           <div className="sidebar__header">
-                <Avatar />   
+            <div className="sidebar__header">
+                <Avatar />
                 <div className="sidebar__headerRight">
-                <DonutLargeIcon />
-                <ChatIcon />
-                <MoreVert />
-            
-            
-                </div>     
-           </div>
-           <div className="sidebar__search">
-               
-           </div>
-           <div className="sidebar__chats">
-               
-           </div>
+                   <IconButton>
+                    <DonutLargeIcon />
+                   </IconButton>
+                   <IconButton>
+                    <ChatIcon />
+                   </IconButton>
+                   <IconButton>
+                    <MoreVert />
+                   </IconButton>
+                </div>
+            </div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start a new chat" type="text" />
+                </div>                
+            </div>
+            <div className="sidebar__chats">
+                <h1>slidebar</h1>
+                <h1>slidebar</h1>
+                <h1>slidebar</h1>
+                <h1>slidebar</h1>
+                <h1>slidebar</h1>
+                <h1>slidebar</h1>
+                <h1>slidebar</h1>
+                <h1>slidebar</h1>
+            </div>
         </div>
     )
 }
